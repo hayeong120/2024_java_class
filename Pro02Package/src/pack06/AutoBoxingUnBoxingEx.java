b960574cfd05057ec892107ee4df33cc4d1a810e
hayeong120 <jbg01200249@gmail.com>
2023-08-19 10:00:00 +0900

    예제 02-6 : 박싱과 언박싱

--- Pro02Package/src/pack06/AutoBoxingUnBoxingEx.java
+++ Pro02Package/src/pack06/AutoBoxingUnBoxingEx.java
@@ -3,10 +3,10 @@ package pack06;
 public class AutoBoxingUnBoxingEx {
 	public static void main(String[] args) {
 		int n = 10;
-		Integer intObject = n;		// auto boxing
+		Integer intObject = n;		// 자동 박싱(auto boxing)
 		System.out.println("intObject = " + intObject);
 		
-		int m = intObject + 10;		// auto unboxing
+		int m = intObject + 10;		// 자동 언박싱(auto unboxing)
 		System.out.println("m = " + m);
 	}
 }
