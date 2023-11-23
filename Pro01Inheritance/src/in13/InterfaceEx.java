030c3e5f9b7b089822848f2fc2a8993252c6fe24
hayeong120 <jbg01200249@gmail.com>
2023-11-23 10:00:00 +0900

    예제 1-13 : 인터페이스 구현

---
+++ Pro01Inheritance/src/in13/InterfaceEx.java
@@ -0,0 +1,11 @@
+package in13;
+
+public class InterfaceEx {
+	public static void main(String[] args) {
+		SamsungPhone phone = new SamsungPhone();
+		phone.printLogo();
+		phone.sendCall();
+		phone.receiveCall();
+		phone.flash();
+	}
+}
