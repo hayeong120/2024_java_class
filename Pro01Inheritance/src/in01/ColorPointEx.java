a6a084f972b385589248f41d714c4121374960ab
hayeong120 <jbg01200249@gmail.com>
2023-12-18 10:00:00 +0900

    예제 1-1 : 클래스 상속 만들기

--- Pro01Inheritance/src/in01/ColorPointEx.java
+++ Pro01Inheritance/src/in01/ColorPointEx.java
@@ -3,10 +3,15 @@ package in01;
 public class ColorPointEx {
 	public static void main(String[] args) {
 		Point p = new Point();		// Point 객체 생성
+		System.out.println(System.identityHashCode(p));		// p가 가리키는 객체의 주소값
+		
+//		p.x = 10; 			// private이기 때문에 오류
 		p.set(1, 2); 			// Point 클래스의 set() 호출
 		p.showPoint();
 		
 		ColorPoint cp = new ColorPoint();	// ColorPoint 객체
+		System.out.println(System.identityHashCode(cp));		// cp가 가리키는 객체의 주소값
+		
 		cp.set(3, 4); 			// Point의 set() 호출
 		cp.setColor("red");	  		// COlorPoint의 setColor() 호출
 		cp.showColorPoint(); 		// 컬러와 좌표 출력
--- Pro01Inheritance/src/in01/Point.java
+++ Pro01Inheritance/src/in01/Point.java
@@ -2,7 +2,7 @@ package in01;
 
 public class Point {
 	private int x, y;	// 한 점을 구성하는 x, y좌표
-	public void set(int x, int y) {
+	public void set(int x, int y) {		// 매개변수 x, y값을 멤버변수 x, y에 저장
 		this.x = x; this.y = y;
 	}
 	public void showPoint() {	// 점의 좌표 출력
